266a8b82b118468e6a4632e30cb98a6b4ffd8234
Matheus Andrade <mathews754@gmail.com>
2020-12-12 01:11:52 -0300

    Código completamente comentado

--- server.js
+++ server.js
@@ -11,16 +11,13 @@ app.post('/parser', function(req, res) {
     codePost = req.body;
 
     //Passando o json, acessando o codigo que foi enviado
-    var codeArray = parser.handleString(codePost.code);
-    var codeArray2 = parser.identifierC(codeArray);
-    var c_use_params = parser.parseCUse(codeArray2);
-    console.log(c_use_params);
-    var c_use = parser.calcCUse(c_use_params);
+    var codeArray = parser.handleString(codePost.code);     // Transforma a string única com o código para um vetor com cada linha do código como um elmento
+    var codeArray2 = parser.identifierC(codeArray);         // Faz um pré parser, define os rótulos e calcula o p-uso 
+    var c_use_params = parser.parseCUse(codeArray2);        // Faz um parser de c-uso e extrai as variáveis necessárias para calcular o c-uso
+    var c_use = parser.calcCUse(c_use_params);              // Cálcula o somatório de c-uso
     console.log(c_use);
 
-    // Cálculo do uso-c aqui
-    // Cálculo do uso-p aqui
-    // Mais o que for necessário
+    // Retorna para o frontend um valor hardcoded
     const out = require('./retorno.json');
     res.status(201).send(out);
 });
--- utils/parser.js
+++ utils/parser.js
@@ -78,7 +78,7 @@ var pUseCount = 0;
 
 // Entrada: | tipo var |
 // Testa corretamente se é um definição
-const isDef = function(matriz_chave_valor, i) {
+const isDef = (matriz_chave_valor, i) => {
     if (matriz_chave_valor[i][2] == "int" || matriz_chave_valor[i][2] == "float" || matriz_chave_valor[i][2] == "char") {
         if (matriz_chave_valor[i+1][1] == "variable") {
             return true;
@@ -89,7 +89,7 @@ const isDef = function(matriz_chave_valor, i) {
 
 // Entrada: | tipo var |
 // Faz parse de definição e adiciona na lista de variaveis
-const parseDef = function(matriz_chave_valor, i) {  
+const parseDef = (matriz_chave_valor, i) => {  
     if (matriz_chave_valor[i][1] == "variable") {
         variaveis.push({
             name: matriz_chave_valor[i][2],
@@ -100,7 +100,7 @@ const parseDef = function(matriz_chave_valor, i) {
 
 // Entrada: | var '=' ...|
 // Testa se está havendo uma atribuição numa variavel corretamente
-const isAttr = function(matriz_chave_valor, i){
+const isAttr = (matriz_chave_valor, i) => {
     if (matriz_chave_valor[i][1] == "variable" && matriz_chave_valor[i+1][1] == 'assignment'
     && (matriz_chave_valor[i+2][1] == "variable" || matriz_chave_valor[i+2][1] == "number" || (matriz_chave_valor[i+2][2] == "-" && matriz_chave_valor.length-1 >= i+3))){
         return true;
@@ -109,24 +109,28 @@ const isAttr = function(matriz_chave_valor, i){
 }
 
 // Entrada: | var '=' ...|
-//TODO: Comentar
-const parseEqu = function(matriz_chave_valor, i){
-    let value = '';
-    let next_i;
-    let v = '';
-    if (matriz_chave_valor[i + 2][2] == "-") {
-        if (matriz_chave_valor[i + 3][1] == "variable") {
+// Extrai informações de uma equação e salva no vetor de variáveis
+const parseEqu = (matriz_chave_valor, i) => {
+    let value = '';     // Serve como acumulador da string de equação para avaliação depois
+    let next_i;         // índice auxiliar para percorrer a matriz
+    let v = '';         // valor temporário para a variável atual
+
+    // Este primeiro if extrairá o primeiro elemento
+    if (matriz_chave_valor[i + 2][2] == "-") {      // Verifica se o primeiro valor começa com '-'
+        if (matriz_chave_valor[i + 3][1] == "variable") {   // E se o próximo rótulo é uma variável
             for (let j in variaveis) {
                  if (variaveis[j].name == matriz_chave_valor[i + 3][2]) {
                      v = variaveis[j].value;
                  }
             }
             value += matriz_chave_valor[i+2][2] + v;
-        } else {
+
+        } else {        // Se não for uma variável (talvez seja uma constante), adiciona o valor com o caractere '-'. Ex: '-3'
             value += matriz_chave_valor[i+2][2] + matriz_chave_valor[i+3][2]
         }
         next_i = i + 4;
-    } else {
+
+    } else {        // Se não vier com um '-', faz a mesma operação do if porém com os índices um pouco alterados (também considera o '-' colado com o valor)
         if (matriz_chave_valor[i + 2][1] == "variable") {
             for (let j in variaveis) { 
                 if (variaveis[j].name == matriz_chave_valor[i + 2][2].replace('-','')) {
@@ -143,9 +147,10 @@ const parseEqu = function(matriz_chave_valor, i){
         }
         next_i = i + 3;
     }
+    // Cria um espaço para o próximo elemento
     value += ' ';
     
-    if (matriz_chave_valor.length-1 <= next_i) {
+    if (matriz_chave_valor.length-1 <= next_i) {    // Caso seja o final da matriz, faz o eval do valor e salva na variável 
         for (let j in variaveis) {
             if (variaveis[j].name == matriz_chave_valor[i][2]) {
                  variaveis[j].value = eval(value);
@@ -154,7 +159,9 @@ const parseEqu = function(matriz_chave_valor, i){
         return;
     }
 
-    while(matriz_chave_valor[next_i][1] == "operacoes") {
+    while(matriz_chave_valor[next_i][1] == "operacoes") {    // enquanto houver operações na sequência da linha, continuará extraindo os valores e salvando em 'value'
+
+        // Faz a mesma operação que o if acima
         if (matriz_chave_valor[next_i+1][2] == "-") {
             if (matriz_chave_valor[next_i+2][1] == "variable") {
                 for (let j in variaveis) {
@@ -167,6 +174,7 @@ const parseEqu = function(matriz_chave_valor, i){
                 value += matriz_chave_valor[next_i][2] + ' ' + matriz_chave_valor[next_i+1][2] + ' ' + matriz_chave_valor[next_i+2][2];
             }
             next_i += 3;
+        // Faz a mesma operação que o else acima (antes do while)
         } else {
             if (matriz_chave_valor[next_i+1][1] == "variable") {
                 for (let j in variaveis) {
@@ -184,6 +192,8 @@ const parseEqu = function(matriz_chave_valor, i){
             }
             next_i += 2;
         }
+
+        // verifica se a matriz chefou ao fim e atribui o valor da equação à variável respectiva
         if (matriz_chave_valor.length-1 <= next_i) {
             for (let j in variaveis) { 
                 if (variaveis[j].name == matriz_chave_valor[i][2]) {
@@ -194,6 +204,7 @@ const parseEqu = function(matriz_chave_valor, i){
         }
         value += ' ';
     }
+    // Atribui o valor da equação final de value à variável respectiva
     for (let j in variaveis) { 
         if (variaveis[j].name == matriz_chave_valor[i][2]) {
              variaveis[j].value = eval(value);
@@ -202,17 +213,19 @@ const parseEqu = function(matriz_chave_valor, i){
     return;
 }
 
-const getCUseParams = function(matriz_chave_valor, linha, start_index){
+const getCUseParams = (matriz_chave_valor, linha, start_index) => {
+    // Extrai as variáveis w, d e c de uma linha no código. 
+
     let i = 0;
-    const line_c_uso = {
+    const line_c_uso = {    // Objeto com o parâmetros do somatório
         w: [],
         d: [],
         c: [],
     }
-    console.log("Start -------------------------------");
-    while(matriz_chave_valor[start_index + i][0] == linha) {
-        console.log("C-Use", matriz_chave_valor[start_index + i]);
-        // Fazer povoamento do w, d e c aqui
+
+    while(matriz_chave_valor[start_index + i][0] == linha) { // O loop percorrerá a linha do código e extrairá o valor dependendo do rótulo
+
+        // Verifica se o rótulo é uma variável
         if (matriz_chave_valor[start_index + i][1] == "variable") {
             let v;
             for (let j in variaveis) { 
@@ -220,35 +233,37 @@ const getCUseParams = function(matriz_chave_valor, linha, start_index){
                     v = variaveis[j].value;
                 }
             }
-            line_c_uso.w.push(v); //.trim() ?
+            line_c_uso.w.push(v);
         }
+
+        // Verifica se o rótulo é um operador
         if (matriz_chave_valor[start_index + i][1] == "operacoes" || matriz_chave_valor[start_index + i][1] == "assignment") {
             line_c_uso.d.push(matriz_chave_valor[start_index + i][2]);
         }
+
+        // Verifica se o rótulo é uma constante
         if (matriz_chave_valor[start_index + i][1] == "number") {
             line_c_uso.c.push(matriz_chave_valor[start_index + i][2]);
         }
 
+        // Verifica se já chegou no fim da matriz para evitar erros fatais
         if (matriz_chave_valor.length-1 == start_index + i + 1) {
             break;
         } else {
             i++;
         }
     }
-    console.log("End -------------------------------");
     return line_c_uso;
 }
 
 // Módulo para transformar json em um  array
 exports.handleString = function (code) {
-    // console.log(code.code);
     // Variavel que armazena as linhas
     var line = '';
 
     // Percorre a string de multiplas linhas e transforma em um array com cada linha do código em
     // uma posição do array, além de fazer a remoção de comentários.
     for (var prop in code) {
-        // if (code[prop] != '\n') {
         if (code[prop] != '{' && code[prop] != ';' && code[prop] != '}') {
             line += code[prop];
         } else {
@@ -269,8 +284,6 @@ exports.handleString = function (code) {
     response = response.filter(function (str) {
         return /\S/.test(str);
     });
-
-    // console.log(resposta);
     return response;
 }
 
@@ -298,7 +311,6 @@ exports.identifierC = function (code) {
 
         // percorrendo item a item da matriz no array aux.
         for (var j = 0; j < aux.length; j++) {
-            // console.log(aux[j]);
 
             if (aux[j] == 'while' || aux[j] == 'if') {
                  pUseCount += 1;    //Contando p-use
@@ -319,7 +331,7 @@ exports.identifierC = function (code) {
             }
             if (aux[j] == '>'){
                 pUse = pUse + esq + 62 + dir + (esq>dir);
-            }                    
+            }
             //********* Fim da parte do calculo do M(p-uso)
 
             if (aux[j] == 'int' || aux[j] == 'while' || aux[j] == 'return' || aux[j] == 'else' || aux[j] == 'if' || aux[j] == 'main' || aux[j] == 'do' || aux[j] == 'float' || aux[j] == 'for') {
@@ -369,11 +381,9 @@ exports.identifierC = function (code) {
 
         linha_com_definicoes = linha_com_definicoes.concat("/*"+count+"*/  ");
         for (var i = 0; i < linha_da_matriz.length; i++) {
-            // console.log(linha_da_matriz);
                 var chave_valor = [];
                 chave_valor.push(count+1);
                 if (reserved_word.includes(linha_da_matriz[i])) {
-                    // linha_com_definicoes = linha_com_definicoes.concat(linha_da_matriz[i]+ ' ');
                     linha_com_definicoes = linha_com_definicoes.concat(' palavra_reservada ');
                     chave_valor.push("palavra_reservada", linha_da_matriz[i], count);
                 }
@@ -382,12 +392,10 @@ exports.identifierC = function (code) {
                     chave_valor.push("parentheses", linha_da_matriz[i], count);
                 }
                 if (variable.includes(linha_da_matriz[i])) {
-                    // linha_com_definicoes = linha_com_definicoes.concat(linha_da_matriz[i]+ ' ');
                     linha_com_definicoes = linha_com_definicoes.concat(' variavel ');
                     chave_valor.push("variable", linha_da_matriz[i], count);
                 } 
                 if (number.includes(linha_da_matriz[i])) {
-                    // linha_com_definicoes = linha_com_definicoes.concat(linha_da_matriz[i]+ ' ');
                     linha_com_definicoes = linha_com_definicoes.concat(' number ');
                     chave_valor.push("number", linha_da_matriz[i], count);
                 } 
@@ -397,7 +405,6 @@ exports.identifierC = function (code) {
                     chave_valor.push("chaves", linha_da_matriz[i], count);
                 }
                 if (name_function.includes(linha_da_matriz[i])) {
-                    // linha_com_definicoes = linha_com_definicoes.concat(linha_da_matriz[i]+ ' ');
                     linha_com_definicoes = linha_com_definicoes.concat(' function ');
                     chave_valor.push("name_function", linha_da_matriz[i], count);
                 }
@@ -424,45 +431,37 @@ exports.identifierC = function (code) {
         linha_com_definicoes = '';
     }
     
-    // Retorno chave_valor;
-    // console.log(matriz_chave_valor);
-    //console.log (`Calculo P-uso: ${pUse}`);
-    console.log("Variáveis: ", variable);
+    // Retorno chave_valor
     return matriz_chave_valor;
-
-    //Retorno com a matriz com definições
-    // return matriz_def;
 }
 
-// !! TODO: Comentar
+// Identifica estruturas e rótulos para extração dos valores de c-uso 
 exports.parseCUse = function(matriz_chave_valor) {
     let linha = 1;
-    // let line = [];
-    // let i = 0;
-
     const lastLine = matriz_chave_valor[matriz_chave_valor.length-1][0];
-    console.log("Lastline: ", lastLine);
 
     let last_i = 0;
     while(linha <= lastLine) {
         let i;
         let isCUse = false;
         for (i = last_i; linha == matriz_chave_valor[i][0]; i++) {
-            console.log(matriz_chave_valor[i]);
+
+            // Quebra o loop caso o índice atual for maior que o tamanho do vetor com os elementos
             if (i == matriz_chave_valor.length-1) {
                 break;
             }
-            // fazer o parser aqui
-            if (isDef(matriz_chave_valor, i)) {      // Detecta se há uma estrutura '| tipo var |' na linha
-                parseDef(matriz_chave_valor, i+1);
-                // console.log(`Def na linha ${linha}: ${matriz_chave_valor[i][2]} ${matriz_chave_valor[i+1][2]}`);
 
+            // Verifica que o rótulo atual é o começo de uma definição de variável
+            if (isDef(matriz_chave_valor, i)) {      // Detecta se há uma estrutura '| tipo var |' na linha
+                parseDef(matriz_chave_valor, i+1);      // Caso detecte uma definição, salvará a variável na memória 
+
+                // Verifica se há uma atribuição depois da definição da variável
                 if (isAttr(matriz_chave_valor, i+1)) {   // Detecta se há uma estrutura '| var '=' ... |' (Se houver, então constitui c-uso)
                     isCUse = true;
-                    parseEqu(matriz_chave_valor, i+1)
-                    // console.log(`Var ${matriz_chave_valor[i][2]} também possui atribuição com o valor ${matriz_chave_valor[i+2][2]}!`);
+                    parseEqu(matriz_chave_valor, i+1)       // Salva o valor da atribuição na variável detectada 
                 }
             }
+            // Verifica se o rótulo é um começo de uma atribuição
             else if (isAttr(matriz_chave_valor, i)) {   // Detecta se há uma estrutura '| var '=' ... |' (Se houver, então constitui c-uso)
                 isCUse = true;
                 parseEqu(matriz_chave_valor, i);
@@ -472,39 +471,44 @@ exports.parseCUse = function(matriz_chave_valor) {
             c_uso.push(getCUseParams(matriz_chave_valor, linha, last_i));
         }
         console.log("\n");
+
         // Faz um update na linha
         last_i = i;
         linha++;
     }
-    console.log(variaveis);
-    console.log(c_uso);
     return c_uso;
 }
 
 exports.calcCUse = function(c_use_params){
-    let s = []
-    let s1, s2, s3;
-    for (param of c_use_params){
-        if (param.w.length > 0) {
+    // c_use_params = Um vetor de objetos com os parâmetros w, d e c; 
+    let s = []          // Vetor que guardará as somas de cada linha;
+    let s1, s2, s3;     // Cada uma dessas variáveis guadará o somatório de um dos parâmetro w, d e c;
+
+    for (param of c_use_params){    // para cada conjunto de parâmetros ele fará o somatório dos 3 parâmetros; 
+
+        if (param.w.length > 0) {   // É preciso verificar se o vetor não está vazio
             s1 = param.w.reduce((acc, value) => {
                 let v = 0;
-                if (typeof value == "string") {
+                if (typeof value == "string") {     // É preciso o tipo da variável para converter e somar o valor certo 
                     for (let char of value) v += parseInt(char.charCodeAt(0), 10);
                     v += acc;
                 }
                 if (typeof value == "number") v = acc + value;
                 return v;
             });
-        } else {
+        } else {    // Caso o vetor seja zero o valor do somatório será 0
             s1 = 0;
         }
-        if (param.d.length > 0) {
+
+        if (param.d.length > 0) {   // É preciso verificar se o vetor não está vazio
             s2 = param.d.reduce((acc, value) => typeof value == "number" ? acc + value : acc + value.charCodeAt(0), 0)
         } else {
             s2 = 0;
-        }
+        }   // Caso o vetor seja zero o valor do somatório será 0
+
         if (param.c.length > 0) {
-            s3 = param.c.reduce((acc, value) => {
+
+            s3 = param.c.reduce((acc, value) => {   // É preciso verificar se o vetor não está vazio
                 let v = 0;
                 if (typeof value == "string") {
                     for (let char of value) v += parseInt(char.charCodeAt(0), 10);
@@ -513,17 +517,19 @@ exports.calcCUse = function(c_use_params){
                 if (typeof value == "number") v = acc + value;
                 return v;
             });
-        } else {
+        } else {    // Caso o vetor seja zero o valor do somatório será 0
             s3 = 0;
         }
         s.push(s1 + s2 + s3);
     }
 
     let count = 0;
-    let sumTotal = 0;
+    let sumTotal = 0;   // Esta variável acumulará o somatório de todos os outros somatórios;
     s.forEach((sum) => {
         console.log("Linha: ", c_use_params[count]);
         console.log(`Soma ${++count}: ${sum.toString(16)}\n`);
         sumTotal += sum;
     });
+
+    return sumTotal;
 }
